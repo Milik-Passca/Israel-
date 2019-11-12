f11ef21c28b923dd9a088e2c07cf3da6488321e8
Milik-Passca <56308222+Milik-Passca@users.noreply.github.com>
2019-11-12 11:07:22 +0100

    Add files via upload

---
+++ Classwork/Assignment2.py
@@ -0,0 +1,6 @@
+from random import randint
+y = randint (1,10)
+print('A random number between 1 and 10: ', y)
+for i in range(y) : 
+    print ("Wilson Passak")
+  
---
+++ Classwork/assignemt5.py
@@ -0,0 +1,5 @@
+#deccimal number 
+import random
+X = random.uniform (1,10)
+print("A random decimal number between 1 and 10 :", X)
+print(format (X, '2f'))
